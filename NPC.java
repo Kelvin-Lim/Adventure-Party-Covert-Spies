21a7b649e7201b0bab265aa73cb8113bf5b32aaf
NotTomFox <ros_tmfox@WROSRM31918.seattleschools.org>
2015-05-15 12:00:02 -0700

    Initial NPC commit

---
+++ NPC.java
@@ -0,0 +1,15 @@
+public class NPC {
+   private String npcName;
+
+
+   public NPC (String name) {
+      npcName = name;
+   }
+   
+   public String getName() {
+      return npcName;
+   }
+   public String toString(){
+      return npcName;
+}
+}
