7d2245fdc4ca2d98d2c2b30bd9ca517f9cf27a7a
JujeFruit <ros_jwtitus@WROSRM31917.seattleschools.org>
2015-05-20 11:54:13 -0700

    Initial Boss Commit

---
+++ Boss.java
@@ -0,0 +1,26 @@
+public class Boss {
+   int bossHp;
+   String bossName;
+   int bossLuck;
+   int[] bossStats;
+   
+   public Boss(int bossHp, String Name, int bossLuck) {
+      this.bossHp = bossHp;
+      this.bossName = bossName;
+      this.bossLuck = bossLuck;
+      bossStats = new int[]{ bossHp, 0};
+      
+   }
+ public String getName() {
+      return bossName;
+   
+   }
+  
+   public int getHp() {
+      return bossStats[0];
+   }
+   
+   public int getLuck() {
+      return bossStats[1];
+   }
+}
