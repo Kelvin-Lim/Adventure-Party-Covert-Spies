8166054a3814698f9ac184dabb2af72ce3090e3d
JujeFruit <ros_jwtitus@WROSRM31917.seattleschools.org>
2015-05-27 11:44:17 -0700

    Attack Update With accuracy.

--- Attack.java
+++ Attack.java
@@ -10,18 +10,50 @@ public class Attack {
       this.damageOf = damageOf;
    }
    
-   public int useAttack(int Hp, int Luck, String attackerName) {
-      int chanceToCrit = Luck * 5;
+   public void useAttackPlayer(Player player, Boss boss) {
+      int strength = damageOf * player.getIntel();
+      int chanceToCrit = player.getLuck() * 5;
+      int accuracy = player.getFocus() * 10;
+      double accurNumber = (Math.random() * 100);
       double critNumber = (Math.random() * 100);
-      System.out.print(attackerName + " used " + nameAtt);
-      if(chanceToCrit >= critNumber) {
-         System.out.println(" for " + (damageOf * 2) + " damage! Critical hit!");
-         Hp-= (damageOf * 2);
-      } else {
-         System.out.println(" for " + damageOf + " damage!");
-         Hp-= damageOf;
+      System.out.print(player.getName() + " used " + nameAtt);
+      if(accuracy >= accurNumber) { 
+         if(chanceToCrit >= critNumber) {
+            System.out.println(" for " + (strength * 2) + " damage! Critical hit!");
+            boss.updateHp(strength * 2);
+            System.out.println(boss.getName() + " has " + boss.getHp() + "HP remaining!");
+         } else {
+            System.out.println(" for " + strength + " damage!");
+            boss.updateHp(strength);
+            System.out.println(boss.getName() + " has " + boss.getHp() + "HP remaining!");
+         }
+      } else { 
+         System.out.println("Oh No! Your attack missed.");
+         System.out.println(boss.getName() + " has " + boss.getHp() + "HP remaining!");
       }
-      return Hp;  
+   }
+   
+   public void useAttackBoss(Player player, Boss boss) {
+      int strength = damageOf;
+      int chanceToCrit = boss.getLuck() * 5;
+      int accuracy = player.getFocus() * 10;
+      double accurNumber = (Math.random() * 100);
+      double critNumber = (Math.random() * 100);
+      System.out.print(boss.getName() + " used " + nameAtt);
+      if(accuracy >= accurNumber) {
+         if(chanceToCrit >= critNumber) {
+            System.out.println(" for " + (strength * 2) + " damage! Critical hit!");
+            player.updateHp(strength * 2);
+            System.out.println(player.getName() + " has " + player.getHp() + "HP remaining!");
+         } else {
+            System.out.println(" for " + strength + " damage!");
+            player.updateHp(strength);
+            System.out.println(player.getName() + " has " + player.getHp() + "HP remaining!");
+         } 
+      } else {
+         System.out.println("You got lucky! Bosses attack missed.");
+         System.out.println(player.getName() + " has " + player.getHp() + "HP remaining!");
+      }
    }
    
    public String getAttackName() {
